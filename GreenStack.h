592a03320271db6a0613f3f3978e8a0ffbe11d63
Sakduh2002 <sakda.tha@student.mahidol.edu>
2025-12-03 15:50:36 +0700

    Create GreenStack.h

---
+++ GreenStack.h
@@ -0,0 +1,52 @@
+#ifndef GREENSTACK_H
+#define GREENSTACK_H
+
+#include "GreenNode.h" 
+#include <iostream>
+
+class GreenStack {
+private:
+    GreenNode* top; 
+
+public:
+    GreenStack() : top(nullptr) {}
+
+    ~GreenStack() {
+        while (!isEmpty()) {
+            pop(); 
+        }
+    }
+
+    void push(std::string name, std::string loc, int year) {
+        GreenNode* newNode = new GreenNode(name, loc, year);
+        newNode->next = top;
+        top = newNode;
+    }
+
+    GreenNode* pop() {
+        if (isEmpty()) return nullptr;
+        
+        GreenNode* temp = top;
+        top = top->next;
+        temp->next = nullptr; 
+        return temp;
+    }
+
+    bool isEmpty() { return top == nullptr; }
+
+    void printStack() {
+        if (isEmpty()) {
+            std::cout << "[Trash Bin is Empty]\n";
+            return;
+        }
+        GreenNode* cur = top;
+        std::cout << "--- TRASH BIN (Recent on top) ---\n";
+        while (cur != nullptr) {
+            std::cout << "[Deleted] " << cur->projectName << "\n";
+            cur = cur->next;
+        }
+        std::cout << "---------------------------------\n";
+    }
+};
+
+#endif
